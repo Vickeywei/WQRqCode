6360f2228f7f71e8b567158a1d6a01f73dbfd71c
wei qi <weiqi@hzdracom.com>
2016-09-12 15:15:17 +0800

    Initial Commit

---
+++ WQQRCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WQQRCode
+//
+//  Created by 魏琦 on 16/9/12.
+//  Copyright © 2016年 com.drcacom.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WQQRCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WQQRCode
+//
+//  Created by 魏琦 on 16/9/12.
+//  Copyright © 2016年 com.drcacom.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
